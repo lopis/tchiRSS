a53193d788ce1b66b991c530f128a72ea21b793a
João Lopes <joaopclopes@gmail.com>
2013-06-16 11:57:00 +0100

    First release. Version 1.0.
    CSS was greatly improved. You can now add feeds andclick on links to
    read the last release or visit the main page of the rss.

--- index.php
+++ index.php
@@ -1,14 +1,85 @@
+<?php 
+
+	include "sqlite.php";
+
+	function compareFeeds($a, $b){
+		return $a['pubDate'] == $b['pubDate']? 0 :
+			$a['pubDate'] > $b['pubDate']? 1 : -1;
+	}
+
+	$sqlite = new SQLiteClass;
+	$sqlite->connect();
+	// $sqlite->clearAll()
+	// $sqlite->insertRSS("http://rss","FEED","feed.png");
+	if(isset($_POST['name']) &&
+		isset($_POST['url']) &&
+		isset($_POST['icon'])){
+		$sqlite->insertRSS($_POST['url'],$_POST['name'],$_POST['icon']);
+	}
+	$feedsPDO = $sqlite->getAll();
+	$feeds = array();
+	foreach ($feedsPDO as $feedPDO) {
+		$feed = array();
+		$feed['id'] = $feedPDO['id'];
+		$feed['name'] = $feedPDO['name'];
+		$feed['icon'] = $feedPDO['icon'];
+		$feed['url'] = $feedPDO['url'];
+		$rss = new SimpleXMLElement($feed['url'], null, true);
+		$lastUpdate = $rss->xpath('channel/item')[0];
+		$feed['title'] = $lastUpdate->title;
+		$feed['link'] = $lastUpdate->link;
+		$feed['pubDate'] = $newDate = floor((time() - strtotime($lastUpdate->pubDate)) / 86400);
+		$feeds[$feed['id']] = $feed;
+	}
+	usort($feeds, 'compareFeeds');
+
+ ?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>RSS Timestamp</title>
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Schoolbell' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<?php
-		echo sqlite_libversion();
-		echo "<br>";
-		echo phpversion();
-	?>
+	<div class="container">
+		<div class="form">
+			<div class="logo">
+				<img src="img/logo.png" alt="">
+			</div>
+			<div class="h2">Add feed</div>
+			<div>
+				<form action="index.php" method="post">
+					<div class="label">Name</div>
+					<div class="input"><input name="name" type="text"></div>
+					<div class="label">URL</div>
+					<div class="label note">E.g.: http://www.starkana.com/rss/Shingeki_no_Kyojin</div>
+					<div class="input"><input name="url" type="text"></div>
+					<div class="label">Icon</div>
+					<div class="input"><input name="icon" type="text"></div>
+					<input type="submit" value="Create">
+				</form>
+			</div>
+		</div>
+		<div class="feeds">
+			<?php foreach ($feeds as $feed){ ?>
+				<div class="feed">
+					<div class="icon"><img src="<?php echo $feed['icon']; ?>" alt=""></div>
+					<div class="details">
+						<div class="name">
+							<a href="<?php echo $feed['url'] ?>"><?php echo $feed['name'] ?></a>
+						</div>
+						<div class="lasttime"><a href="<?php echo $feed['link'] ?>">Last realease</a>: 
+							<?php
+								echo $feed['pubDate'] < 1 ? "Today!" :
+									 $feed['pubDate']== 1 ? "Yesterday" : $feed['pubDate'] . " days ago";
+							?>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
 </body>
 </html>
---
+++ sqlite.php
@@ -0,0 +1,46 @@
+<?php
+
+class SQLiteClass {
+
+	public $dbname = "rss";
+	public $file = "rss.sqlite3";
+    public $db;
+
+    public function connect() {
+        try {
+        	$this->db = new PDO(
+        		"sqlite:".($this->file));
+        	$this->db->exec("CREATE TABLE IF NOT EXISTS feeds(
+        		id INTEGER PRIMARY KEY,
+        		url TEXT,
+        		name TEXT,
+        		icon TEXT,
+        		lastTime INTEGER DEFAULT 0,
+        		lastIndex INTEGER DEFAULT 0)");
+
+        } catch (PDOException $e) {
+        	echo $e->getMessage();
+        }
+    }
+
+    public function insertRSS($url,$name,$icon){
+    	$insert = "INSERT INTO feeds(url, name, icon)
+    		VALUES (:url, :name, :icon)";
+    	$stmt = $this->db->prepare($insert);
+		$stmt->bindValue(':url', $url, SQLITE3_TEXT);
+		$stmt->bindValue(':name', $name, SQLITE3_TEXT);
+		$stmt->bindValue(':icon', $icon, SQLITE3_TEXT);
+      	return $stmt->execute();
+    }
+
+    public function getAll(){
+    	return $this->db->query('SELECT * FROM feeds');
+    }
+
+    public function clearAll(){
+    	$this->db->exec("DROP TABLE feeds");
+    	$this->connect();
+    }
+}
+
+?>
