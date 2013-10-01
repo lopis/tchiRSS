947e64f31bfdf4722e7bd62a402549b27f1f409c
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-01 18:42:44 +0100

    Added delete feed feature. Minor fixes.
    Added delete feed feature which includes SQL, PHP and javascript
    functions. Corrected minor code-smells (changed some == to ===).
    Corrected calling ajax with inline data. Added html5 attribute data-id
    to each feed. Added javascript utility functions to remove feeds.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var feeds;
 
 $(document).ready(function() {
-	console.log("DOM is ready");
+	//console.log("DOM is ready");
 	getFeeds();
+	configureHandlers();
 });
+
+function configureHandlers () {
+	$('.feeds').on('click', '.delete', deleteFeed);
+}
 
 function updateFeeds(){
 	feedNum = feeds.length;
 	$.each(feeds, function(feed){
 		$.ajax({
-			url: "getfeeds.php?feed=" + feeds[feed]['id'],
+			url: 'getfeeds.php',
+			data: { feed: feeds[feed]['id'] },
 			success: function(result){
 				xmlDoc = $.parseXML(result),
 				xml = $( xmlDoc ),
@@ -23,7 +29,7 @@ function updateFeeds(){
 				//$("#"+feeds[feed]['id']+" > .details > .lasttime > span").text(str);
 				//putFeed(feed);
 				feedNum--;
-				if (feedNum == 0) {
+				if (feedNum === 0) {
 					sortFeeds();
 					$.each(feeds, function(f){
 						putFeed(f);
@@ -54,24 +60,24 @@ function sortFeeds(){
 
 function putFeed(feed){
 	id = feeds[feed]['id'];
-	$(".feeds").append("<div class='feed' id='" + id + "'/>");
+	$(".feeds").append("<div class='feed' id='" + id + "' data-id='" + id + "'/>");
 
-	$(".feed#"+id).append("<div class='icon'/>");
-	$(".feed#"+id).append("<div class='details'/>");
-	$(".feed#"+id+" .details").append("<div class='links'/>");
-	$(".feed#"+id+" .details").append("<div class='name'/>");
-	$(".feed#"+id+" .details").append("<div class='lastime'/>");
-	$(".feed#"+id+" .lastime").append("<span/>");
+	$(".feed#" + id).append("<div class='icon'/>");
+	$(".feed#" + id).append("<div class='details'/>");
+	$(".feed#" + id + " .details").append("<div class='links'/>");
+	$(".feed#" + id + " .details").append("<div class='name'/>");
+	$(".feed#" + id + " .details").append("<div class='lastime'/>");
+	$(".feed#" + id + " .lastime").append("<span/>");
 
-	$(".feed#"+id+" .icon").append("<img src='"+feeds[feed]['icon']+"' alt='' />");
-	$(".feed#"+id+" .name").append(feeds[feed]['name']);
-	$(".feed#"+id+" .lastime > span").append(
-		"<span class='lasttime'>Last release: </span>"
-		+ "<a href='"+feeds[feed]['lastLink']+"'>"+timeAgo(feeds[feed]['timeSince'])+"</a>"
+	$(".feed#" + id + " .icon").append("<img src='" + feeds[feed]['icon'] + "' alt='' />");
+	$(".feed#" + id + " .name").append(feeds[feed]['name']);
+	$(".feed#" + id + " .lastime > span").append(
+		"<span class='lasttime'>Last release: </span>"  +
+		"<a href='" + feeds[feed]['lastLink'] + "'>" + timeAgo(feeds[feed]['timeSince']) + "</a>"
 	);
-	$(".feed#"+id+" .links").append('<div class="btn edit"></div>');
-	$(".feed#"+id+" .links").append('<div class="btn delete"></div>');
-	$(".feed#"+id+" .links").append('<div class="btn rss"></div>');
+	$(".feed#" + id + " .links").append('<div class="btn edit"></div>');
+	$(".feed#" + id + " .links").append('<div class="btn delete" data-id="' + id + '"></div>');
+	$(".feed#" + id + " .links").append('<div class="btn rss"></div>');
 
 }
 
@@ -82,8 +88,22 @@ function timeAgo(num){
 		case 1:
 			return "Yesterday";
 		default:
-			return num + " days ago"
+			return num + " days ago";
 	}
+}
+
+/**
+ * Removes all feeds from the DOM
+ */
+function clearFeedsDOM () {
+	$('.feeds').html('');
+}
+
+/**
+ * Removes a feed from the DOM
+ */
+function removeFeedDOM (id) {
+	$('#' + id + '.feed').remove();
 }
 
 function getFeeds(){
@@ -93,5 +113,32 @@ function getFeeds(){
 	}).done( function(result){
 		feeds = result;
 		updateFeeds();
+	});
+}
+
+
+/**
+ * Event handler to remove a feed.
+ * Calls the deleteFeed.php to remove the feed.
+ */
+function deleteFeed(evt){
+	var proceed = confirm("Are you sure?");
+	if(!proceed) return;
+
+	var id = evt.target.getAttribute('data-id');
+
+	$.ajax({
+		type: 'POST',
+		url: 'deleteFeed.php',
+		data: {id : id},
+		dataType: 'json'
+	}).done( function(result){
+		if(result.success === 1) {
+			removeFeedDOM(id);
+			//clearFeeds();
+			//getFeeds();
+		} else {
+			console.log('Could not remove feed.');
+		}
 	});
 }
--- sqlite.php
+++ sqlite.php
@@ -4,52 +4,71 @@ class SQLiteClass {
 
 	public $dbname = "rss";
 	public $file = "rss.sqlite3";
-    public $db;
+	public $db;
 
-    public function connect() {
-        try {
-        	$this->db = new PDO(
-        		"sqlite:".($this->file));
-        	$this->db->exec("CREATE TABLE IF NOT EXISTS feeds(
-        		id INTEGER PRIMARY KEY,
-        		url TEXT,
-        		name TEXT,
-        		icon TEXT,
-        		lastTime INTEGER DEFAULT 0,
-        		lastIndex INTEGER DEFAULT 0)");
+	public function connect() {
+		try {
+			$this->db = new PDO(
+				"sqlite:".($this->file));
+			$this->db->exec("CREATE TABLE IF NOT EXISTS feeds(
+				id INTEGER PRIMARY KEY,
+				url TEXT,
+				name TEXT,
+				icon TEXT,
+				lastTime INTEGER DEFAULT 0,
+				lastIndex INTEGER DEFAULT 0)");
 
-        } catch (PDOException $e) {
-        	echo $e->getMessage();
-        }
-    }
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
 
-    public function insertRSS($url,$name,$icon){
-    	$insert = "INSERT INTO feeds(url, name, icon)
-    		VALUES (:url, :name, :icon)";
-    	$stmt = $this->db->prepare($insert);
+	public function insertRSS($url,$name,$icon){
+		$insert = "INSERT INTO feeds(url, name, icon)
+			VALUES (:url, :name, :icon)";
+		$stmt = $this->db->prepare($insert);
 		$stmt->bindValue(':url', $url, SQLITE3_TEXT);
 		$stmt->bindValue(':name', $name, SQLITE3_TEXT);
 		$stmt->bindValue(':icon', $icon, SQLITE3_TEXT);
-      	return $stmt->execute();
-    }
+		return $stmt->execute();
+	}
 
-    public function getAll(){
-    	return $this->db->query('SELECT * FROM feeds');
-    }
+	public function getAll(){
+		return $this->db->query('SELECT * FROM feeds');
+	}
 
-    public function getURL($feedID){
-        $select = "SELECT url FROM feeds WHERE id = :id";
-        $stmt = $this->db->prepare($select);
-        $stmt->bindValue(':id', $feedID);
-        $stmt->execute();
-        $result = $stmt->fetch();
-        return $result[0];
-    }
+	public function getURL($feedID){
+		$select = "SELECT url FROM feeds WHERE id = :id";
+		$stmt = $this->db->prepare($select);
+		$stmt->bindParam(':id', $feedID);
+		$stmt->execute();
+		$result = $stmt->fetch();
+		return $result[0];
+	}
 
-    public function clearAll(){
-    	$this->db->exec("DROP TABLE feeds");
-    	$this->connect();
-    }
+	public function clearAll(){
+		$this->db->exec("DROP TABLE feeds");
+		$this->connect();
+	}
+
+	/**
+	 * deleteFeed
+	 *
+	 * @return void
+	 * @author João Gonçalves
+	 */
+	public function deleteFeed($feedID) {
+		try {
+			$sql = "DELETE FROM feeds WHERE id = :id";
+			$stmt = $this->db->prepare($sql);
+			$stmt->bindValue(':id', $feedID);
+			$stmt->execute();
+			$count = $stmt->rowCount();
+			return $count;
+		} catch (PDOException $e) {
+			return -1;
+		}
+	}
 }
 
 ?>
