d8f3616e7d525fe025234e9a66f5b304c7d8191b
João Gonçalves <jsvgoncalves@gmail.com>
2013-10-02 03:29:47 +0100

    Code refactoring.
    Changed javascript loading to the end of HTML (no need for
    onDocumentReady ...).
    Changed overall structure of js file. Now there is a static variable
    called tchiRSS which contains all the information.

--- script.js
+++ script.js
@@ -1,85 +1,136 @@
-var feeds;
+//var feeds;
 
-$(document).ready(function() {
-	//console.log("DOM is ready");
-	getFeeds();
-	configureHandlers();
-});
+var tchiRSS = {
+	init: function ( config ) {
+		this.config = config;
+		this.feeds;
 
-function configureHandlers () {
-	$('.feeds').on('click', '.delete', deleteFeed);
-}
+		this.attachHandlers();
+		this.getFeeds();
+	},
 
-function updateFeeds(){
-	feedNum = feeds.length;
-	$.each(feeds, function(feed){
-		$.ajax({
-			url: 'getfeeds.php',
-			data: { feed: feeds[feed]['id'] },
-			success: function(result){
-				xmlDoc = $.parseXML(result),
-				xml = $( xmlDoc ),
-				feeds[feed]['lastLink'] = xml.find( "item:first-of-type > link" ).text();
-				pubDate = xml.find( "item:first-of-type > pubDate" ).text();
-				lastRelease = new Date(pubDate);
-				timeNow = new Date();
-				timeSince = Math.round((timeNow.getTime() - lastRelease.getTime())/86400000);
-				feeds[feed]['timeSince'] = timeSince;
-				str = timeAgo(timeSince);
-				//$("#"+feeds[feed]['id']+" > .details > .lasttime > span").text(str);
-				//putFeed(feed);
-				feedNum--;
-				if (feedNum === 0) {
-					sortFeeds();
-					$.each(feeds, function(f){
-						putFeed(f);
-					});
+	attachHandlers: function () {
+		this.config.feedsContainer.on('click', '.delete', tchiRSS.deleteFeed);
+	},
+	
+	updateFeeds: function () {
+		var self = this,
+			feeds = self.feeds;
+
+		feedNum = feeds.length;
+		$.each(feeds, function(feed){
+			$.ajax({
+				url: self.config.feedsSRC,
+				data: { feed: feeds[feed]['id'] },
+				success: function(result){
+					xmlDoc = $.parseXML(result),
+					xml = $( xmlDoc ),
+					feeds[feed]['lastLink'] = xml.find( "item:first-of-type > link" ).text();
+					pubDate = xml.find( "item:first-of-type > pubDate" ).text();
+					lastRelease = new Date(pubDate);
+					timeNow = new Date();
+					timeSince = Math.round((timeNow.getTime() - lastRelease.getTime())/86400000);
+					feeds[feed]['timeSince'] = timeSince;
+					str = timeAgo(timeSince);
+					//$("#"+feeds[feed]['id']+" > .details > .lasttime > span").text(str);
+					//putFeed(feed);
+					feedNum--;
+					if (feedNum === 0) {
+						self.sortFeeds();
+						$.each(feeds, function(f){
+							self.putFeed(f);
+						});
+					}
 				}
+			});
+		});
+	},
+
+	sortFeeds: function () {
+		this.feeds.sort(function(a,b){
+			at = a['timeSince'];
+			bt = b['timeSince'];
+			return at<bt?-1:at>bt?1:0;
+		});
+	},
+
+	putFeed: function (feed) {
+		id = this.feeds[feed]['id'];
+		$(".feeds").append("<div class='feed' id='" + id + "' data-id='" + id + "'/>");
+
+		$(".feed#" + id).append("<div class='icon'/>");
+		$(".feed#" + id).append("<div class='details'/>");
+		$(".feed#" + id + " .details").append("<div class='links'/>");
+		$(".feed#" + id + " .details").append("<div class='name'/>");
+		$(".feed#" + id + " .details").append("<div class='lastime'/>");
+		$(".feed#" + id + " .lastime").append("<span/>");
+
+		$(".feed#" + id + " .icon").append("<img src='" + this.feeds[feed]['icon'] + "' alt='' />");
+		$(".feed#" + id + " .name").append(this.feeds[feed]['name']);
+		$(".feed#" + id + " .lastime > span").append(
+			"<span class='lasttime'>Last release: </span>"  +
+			"<a href='" + this.feeds[feed]['lastLink'] + "'>" + timeAgo(this.feeds[feed]['timeSince']) + "</a>"
+		);
+		$(".feed#" + id + " .links").append('<div class="btn edit"></div>');
+		$(".feed#" + id + " .links").append('<div class="btn delete" data-id="' + id + '"></div>');
+		$(".feed#" + id + " .links").append('<div class="btn rss"></div>');
+	},
+
+	getFeeds: function () {
+		var self = this;
+		$.ajax({
+			type: 'POST',
+			url: self.config.feedsSRC,
+			dataType: 'json'
+		}).done( function(result){
+			self.feeds = result;
+			self.updateFeeds();
+		});
+	},
+
+	/**
+	 * Event handler to remove a feed.
+	 * Calls the deleteFeed.php to remove the feed.
+	 */
+	deleteFeed: function (evt) {
+		var proceed = confirm("Are you sure?");
+		if(!proceed) return;
+
+		var id = evt.target.getAttribute('data-id'),
+			self = tchiRSS;
+
+		$.ajax({
+			type: 'POST',
+			url: 'deleteFeed.php',
+			data: {id : id},
+			dataType: 'json'
+		}).done( function(result){
+			if(result.success === 1) {
+				self.removeFeedDOM(id);
+				//clearFeeds();
+				//getFeeds();
+			} else {
+				console.log('Could not remove feed.');
 			}
 		});
-	});
+	},
 
-/*	feeds.sort(function(a,b){
-		at = a['timeSince'];
-		bt = b['timeSince'];
-		return at<bt?-1:at>bt?1:0;
-	});*/
+	/**
+	 * Removes all feeds from the DOM
+	 */
+	clearFeedsDOM: function () {
+		$('.feeds').html('');
+	},
 
-/*	$.each(feeds, function(feed){
+	/**
+	 * Removes a feed from the DOM
+	 */
+	removeFeedDOM: function (id) {
+		$('#' + id + '.feed').remove();
+	}
 
-	});*/
-}
+};
 
-function sortFeeds(){
-	feeds.sort(function(a,b){
-		at = a['timeSince'];
-		bt = b['timeSince'];
-		return at<bt?-1:at>bt?1:0;
-	});
-}
-
-function putFeed(feed){
-	id = feeds[feed]['id'];
-	$(".feeds").append("<div class='feed' id='" + id + "' data-id='" + id + "'/>");
-
-	$(".feed#" + id).append("<div class='icon'/>");
-	$(".feed#" + id).append("<div class='details'/>");
-	$(".feed#" + id + " .details").append("<div class='links'/>");
-	$(".feed#" + id + " .details").append("<div class='name'/>");
-	$(".feed#" + id + " .details").append("<div class='lastime'/>");
-	$(".feed#" + id + " .lastime").append("<span/>");
-
-	$(".feed#" + id + " .icon").append("<img src='" + feeds[feed]['icon'] + "' alt='' />");
-	$(".feed#" + id + " .name").append(feeds[feed]['name']);
-	$(".feed#" + id + " .lastime > span").append(
-		"<span class='lasttime'>Last release: </span>"  +
-		"<a href='" + feeds[feed]['lastLink'] + "'>" + timeAgo(feeds[feed]['timeSince']) + "</a>"
-	);
-	$(".feed#" + id + " .links").append('<div class="btn edit"></div>');
-	$(".feed#" + id + " .links").append('<div class="btn delete" data-id="' + id + '"></div>');
-	$(".feed#" + id + " .links").append('<div class="btn rss"></div>');
-
-}
 
 function timeAgo(num){
 	switch(num){
@@ -92,53 +143,8 @@ function timeAgo(num){
 	}
 }
 
-/**
- * Removes all feeds from the DOM
- */
-function clearFeedsDOM () {
-	$('.feeds').html('');
-}
 
-/**
- * Removes a feed from the DOM
- */
-function removeFeedDOM (id) {
-	$('#' + id + '.feed').remove();
-}
-
-function getFeeds(){
-	$.ajax({
-		url: "getfeeds.php",
-		dataType: "json"
-	}).done( function(result){
-		feeds = result;
-		updateFeeds();
-	});
-}
-
-
-/**
- * Event handler to remove a feed.
- * Calls the deleteFeed.php to remove the feed.
- */
-function deleteFeed(evt){
-	var proceed = confirm("Are you sure?");
-	if(!proceed) return;
-
-	var id = evt.target.getAttribute('data-id');
-
-	$.ajax({
-		type: 'POST',
-		url: 'deleteFeed.php',
-		data: {id : id},
-		dataType: 'json'
-	}).done( function(result){
-		if(result.success === 1) {
-			removeFeedDOM(id);
-			//clearFeeds();
-			//getFeeds();
-		} else {
-			console.log('Could not remove feed.');
-		}
-	});
-}
+tchiRSS.init( {
+	feedsContainer : $('.feeds'),
+	feedsSRC: 'getfeeds.php'
+});
